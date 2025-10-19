2ec2208ae3a48a79490842258f411b3cca6401cf
sysnote8main <sysnote8main@gmail.com>
2025-10-19 20:22:50 +0900

    feat: add easy json unmarshal wrapper

---
+++ cli/internal/simplejson/basic.go
@@ -0,0 +1,25 @@
+package simplejson
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func Unmarshal[T any](data []byte) (result T, err error) {
+	err = json.Unmarshal(data, &result)
+	return
+}
+
+// UnmarshalResponse returns unmarshaled data.
+//
+// CAUTION: This function automatically close response body so can't use body later.
+func UnmarshalResponse[T any](response *http.Response) (result T, err error) {
+	defer response.Body.Close()
+	b, err := io.ReadAll(response.Body)
+	if err != nil {
+		return
+	}
+	result, err = Unmarshal[T](b)
+	return
+}
