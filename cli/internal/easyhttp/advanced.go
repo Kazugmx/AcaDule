dcb749e40f76bd1c6425b36a13a7d2ef1438d320
sysnote8main <sysnote8main@gmail.com>
2025-10-19 23:09:02 +0900

    feat: add post function with bearer token

--- cli/internal/easyhttp/advanced.go
+++ cli/internal/easyhttp/advanced.go
@@ -9,6 +9,20 @@ func PostJsonBytes(url string, body []byte) (*http.Response, error) {
 	return httpClient.Post(url, "application/json", bytes.NewBuffer(body))
 }
 
+func PostJsonWithBearer(url, token string, body []byte) (response *http.Response, err error) {
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(body))
+	if err != nil {
+		return
+	}
+
+	// add header
+	AcceptJson(req)
+	AuthBearer(req, token)
+
+	response, err = Do(req)
+	return
+}
+
 func GetJsonWithBearer(url, token string) (res *http.Response, err error) {
 	// create request
 	req, err := http.NewRequest(http.MethodGet, url, nil)
