c28637f872d60d2855c77a90dfdd7a5b5d46cfc5
sysnote8main <sysnote8main@gmail.com>
2025-10-19 21:13:02 +0900

    feat: implement list command and write short help msg

--- cli/cmd/task.go
+++ cli/cmd/task.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"acadule-cli/internal/acaduleapi"
+	"acadule-cli/internal/config"
 	"fmt"
 	"os"
 
@@ -9,7 +11,8 @@ import (
 
 var (
 	taskCmd = &cobra.Command{
-		Use: "task",
+		Use:   "task",
+		Short: "Edit tasks in AcaDule platform",
 		Run: func(cmd *cobra.Command, args []string) {
 			err := cmd.Help()
 			if err != nil {
@@ -19,24 +22,29 @@ var (
 		},
 	}
 	listCmd = &cobra.Command{
-		Use: "list",
-		Run: doList,
+		Use:   "list",
+		Short: "Show task list",
+		Run:   doList,
 	}
 	addCmd = &cobra.Command{
-		Use: "add",
-		Run: doAdd,
+		Use:   "add",
+		Short: "Add the task",
+		Run:   doAdd,
 	}
 	viewCmd = &cobra.Command{
-		Use: "view",
-		Run: doView,
+		Use:   "view",
+		Short: "View detail of the task",
+		Run:   doView,
 	}
 	updateCmd = &cobra.Command{
-		Use: "update",
-		Run: doUpdate,
+		Use:   "update",
+		Short: "Update the task data",
+		Run:   doUpdate,
 	}
 	deleteCmd = &cobra.Command{
-		Use: "delete",
-		Run: doDelete,
+		Use:   "delete",
+		Short: "Delete the task",
+		Run:   doDelete,
 	}
 )
 
@@ -52,7 +60,25 @@ func init() {
 }
 
 func doList(cmd *cobra.Command, args []string) {
+	cfg, err := config.Load()
+	if err != nil {
+		fmt.Println("Error occurred on loading config:", err)
+		os.Exit(1)
+	}
+	validateAndUpdateConfig(&cfg)
 
+	fmt.Println("Fetching status...")
+
+	allTasks, err := acaduleapi.GetAll(apiURL, cfg.Token)
+	if err != nil {
+		fmt.Println("Failed to get tasks:", err)
+		os.Exit(1)
+	}
+
+	for _, d := range *allTasks {
+		fmt.Printf("- %s: %s\n", d.Id, d.Title)
+	}
+	fmt.Printf("Count: %d\n", len(*allTasks))
 }
 
 func doAdd(cmd *cobra.Command, args []string) {
